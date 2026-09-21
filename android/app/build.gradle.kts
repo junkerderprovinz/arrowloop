13debe71f435123fd7539dfe6459f26c03e2e0a5
Junker der Provinz <jdp@halleluja.design>
2026-09-21 22:00:27 +0200

    fix(deps): update dependency androidx.appcompat:appcompat to v1.8.0 (#25)
    Co-authored-by: junkerderprovinz <jdp@braethoria.com>

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -85,7 +85,7 @@ android {
 
 dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
-    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.appcompat:appcompat:1.8.0")
     implementation("androidx.activity:activity-ktx:1.9.3")
     implementation("androidx.documentfile:documentfile:1.0.1")
     implementation("androidx.work:work-runtime-ktx:2.10.0")
